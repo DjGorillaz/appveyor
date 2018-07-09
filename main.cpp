e6717e26bfa387398280025c6098622bd4df4a06
Unknown <djg2008@gmail.com>
2018-07-09 22:18:46 +0300

    fix .yml

--- main.cpp
+++ main.cpp
@@ -2,9 +2,12 @@
 #include <QApplication>
 #include <string_view>
 #include <iostream>
+#include <Eigen/Dense>
 
 int main(int argc, char *argv[])
 {
+    int N = 10;
+    Eigen::ArrayXXd sample(N,7);
     std::string str{"Hello world!"};
     std::string_view sw(str);
     std::cout << sw << std::endl;
